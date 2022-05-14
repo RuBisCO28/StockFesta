a2d34877e335b44656622a3b238f9fd42066ef83
RuBiCO28 <54470624+RuBisCO28@users.noreply.github.com>
2022-05-14 21:22:56 +0900

    Install tailwind and setup header&footer

---
+++ layouts/page.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+</script>
+
+<template>
+  <div class="flex flex-col min-h-screen">
+    <slot name="header">
+      <Header />
+    </slot>
+    <slot />
+    <slot name="footer">
+      <Footer />
+    </slot>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'page',
+})
+</script>
+
+<template>
+  <h1 class=" font-bold underline">
+    Hello world!
+  </h1>
+</template>
